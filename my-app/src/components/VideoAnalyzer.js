accd0ec30408ff3731606dd28522acb693cb303a
Mitchell <mitchell.rian.smith@gmail.com>
2020-07-19 10:22:57 -0700

    Add comments on how to issolate parts of an image onto a canvas

--- my-app/src/components/VideoAnalyzer.js
+++ my-app/src/components/VideoAnalyzer.js
@@ -5,47 +5,50 @@ class VideoAnalyzer extends Component {
   	constructor(props){
 	    super(props);
 	    this.state = {
-	    	
 
     	};
     	this.frameRef = React.createRef();
     	this.canvasRef = React.createRef();
+    	this.canvasClick = this.canvasClick.bind();
 
   	}
 
   	componentDidMount(){
 
-  	/*	const can = this.refs.can
-   		const ctx = can.getContext("2d")
-    	const img = this.refs.image
-
-    	img.onload = () => {
-	      	ctx.drawImage(img, 0, 0)
-	      	ctx.font = "40px Courier"
-	      	
-	    }*/
-	    //console.log("hello world");
-	    //console.log(this.canvasRef.current);
 
 	    const frame = this.canvasRef.current;
-	    const ctx = frame.getContext('2d');
+	    const brush = frame.getContext('2d');
 	    const f = this.frameRef.current;
 
 	    f.onload = () => {
-		    ctx.drawImage(f, 0, 0);
-		    //ctx.font = "40px Courier"
-		    //ctx.fillText(this.props.text, 210, 75)
+	    	console.log(brush);
+		    brush.drawImage(f, 50, 50, 50, 50, 0, 0, 50, 50); 
+		//DEV NOTE on Canvas Drawings: 
+		//		- Using drawImage(f, 50, 50, 50, 50) will start drawing on the canvas at point 50,50 and will squish the full image in a 50x50 box
+		//			- Shifts the image to 50,50 and displays the image in a 50x50 box
+		//		- Using drawImage(f, 100, 100) will start drawing the canvas at point 100,100 from point 0, 0 of the image, and will will the canvas from that point
+		//			- Shifts the image to 100,100 on the canvas, and keeps the original size of the image
+		//drawImage Syntax (https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage)
+		//	void ctx.drawImage(image, dx, dy);
+		//	void ctx.drawImage(image, dx, dy, dWidth, dHeight);
+		//	void ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+
 		}
 
 	    
   	}
-  	// <canvas ref="can" width={640} height={425} />
+  	canvasClick(event){
+  		console.log("canvas clicked");
+  		
+  		console.log(event.clientX );
+  		console.log(event.clientY);
+  	}
   	
  	render () {
 	    return ( 
 	    	<div>
-        		<img ref={this.frameRef} src={require('./example-informationLocation.jpg')} alt='example' className="hidden"/>
-        		<canvas ref={this.canvasRef} width={1920} height={1080} style={{border:'1px solid #000000'}} />
+        		<img ref={this.frameRef} src={require('./example-informationLocation.jpg')} alt='example' style={{display: 'none'}}/>
+        		<canvas ref={this.canvasRef} width={1920} height={1080} style={{border:'1px solid #000000'}} onClick={this.canvasClick} />
 	    	</div> 
 	    );
 	}
